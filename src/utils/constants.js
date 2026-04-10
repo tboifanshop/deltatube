f1843304cbaab51fd6baa76ed07241aef671eeb7
tboifan <szymkowiaknatan22@gmail.com>
2026-04-10 19:38:47 +0200

    Refactor categories array and add 'Shorts' category

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -14,20 +14,21 @@ import PersonIcon from '@mui/icons-material/Person';
 export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
 
 export const categories = [
-  { name: 'Deltarune',           icon: <HomeIcon /> },
-  { name: 'Deltarune OST',       icon: <MusicNoteIcon /> },
-  { name: 'Deltarune Chapter 1', icon: <LooksOneIcon /> },
-  { name: 'Deltarune Chapter 2', icon: <LooksTwoIcon /> },
-  { name: 'Deltarune gameplay',  icon: <SportsEsportsIcon /> },
-  { name: 'Deltarune animation', icon: <OndemandVideoIcon /> },
-  { name: 'Deltarune lore',      icon: <SchoolIcon /> },
-  { name: 'Deltarune Susie',     icon: <PersonIcon /> },
-  { name: 'Deltarune Ralsei',    icon: <PersonIcon /> },
-  { name: 'Deltarune Kris',      icon: <PersonIcon /> },
-  { name: 'Deltarune Dark World', icon: <DarkModeIcon /> },
-  { name: 'Deltarune fan made',  icon: <StarIcon /> },
+  { name: 'Deltarune',            icon: <HomeIcon /> },
+  { name: 'Deltarune OST',        icon: <MusicNoteIcon /> },
+  { name: 'Deltarune Chapter 1',  icon: <LooksOneIcon /> },
+  { name: 'Deltarune Chapter 2',  icon: <LooksTwoIcon /> },
+  { name: 'Deltarune gameplay',   icon: <SportsEsportsIcon /> },
+  { name: 'Shorts',               icon: <OndemandVideoIcon /> },
+  { name: 'Deltarune animation',  icon: <OndemandVideoIcon /> },
+  { name: 'Deltarune lore',       icon: <SchoolIcon /> },
+  { name: 'Deltarune Susie',      icon: <PersonIcon /> },
+  { name: 'Deltarune Ralsei',     icon: <PersonIcon /> },
+  { name: 'Deltarune Kris',       icon: <PersonIcon /> },
+  { name: 'Deltarune Dark World',  icon: <DarkModeIcon /> },
+  { name: 'Deltarune fan made',   icon: <StarIcon /> },
   { name: 'Deltarune soundtrack', icon: <GraphicEqIcon /> },
-  { name: 'Toby Fox',            icon: <AudiotrackIcon /> },
+  { name: 'Toby Fox',             icon: <AudiotrackIcon /> },
 ];
 
 export const demoThumbnailUrl = 'https://i.ibb.co/G2L2Gwp/API-Course.png';
@@ -35,4 +36,4 @@ export const demoChannelUrl = '/channel/UCmXmlB4-HJytD7wek0Uo97A';
 export const demoVideoUrl = '/video/GDa8kZLNhJ4';
 export const demoChannelTitle = 'JavaScript Mastery';
 export const demoVideoTitle = 'Build and Deploy 5 JavaScript & React API Projects in 10 Hours - Full Course | RapidAPI';
-export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
+export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png';
